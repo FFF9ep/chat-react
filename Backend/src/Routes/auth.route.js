ed28d20617370166f09a344d2fb4991662f29e0c
fandi a <fandiiardy@gmail.com>
2024-12-12 22:54:24 +0700

    update

--- Backend/src/Routes/auth.route.js
+++ Backend/src/Routes/auth.route.js
@@ -2,4 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/signup", (req, res) => {
+  res.send("signup routes");
+});
+
+router.get("/login", (req, res) => {
+  res.send("login routes");
+});
+
+router.get("/logout", (req, res) => {
+  res.send("logout routes");
+});
+
 export default router;
