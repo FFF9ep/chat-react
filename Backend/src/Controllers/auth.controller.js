3378dd39f299042547fb38d97e560fe8e132fd45
fandi a <fandiiardy@gmail.com>
2024-12-13 04:14:15 +0700

    creating hashing password

--- Backend/src/Controllers/auth.controller.js
+++ Backend/src/Controllers/auth.controller.js
@@ -1,5 +1,36 @@
+import User from "../Models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.send("signup routes");
+  const { email, fullName, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password:hashedPassword,
+    });
+
+    if (newUser) {
+      
+    }else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = async (req, res) => {
