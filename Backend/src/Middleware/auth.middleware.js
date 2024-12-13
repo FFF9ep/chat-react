c91bafbef4a37e5e71ece33bbd069daec3bcb8dd
fandi a <fandiiardy@gmail.com>
2024-12-13 19:45:20 +0700

    finishing auth middleware

--- Backend/src/Middleware/auth.middleware.js
+++ Backend/src/Middleware/auth.middleware.js
@@ -10,11 +10,20 @@ export const protectRoute = async (req, res, next) => {
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.userId);
+        if (!decoded) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
         req.user = user;
         next();
-        
+
     } catch (error) {
-        
+        console.log("Error in protectRoute middleware", error.message);
+        res.status(500).json({ message: "Internal server error" });
     }
 }
