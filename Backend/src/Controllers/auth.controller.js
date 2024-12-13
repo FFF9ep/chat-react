4280dfbefe35bf94e4c6a35575bc7999bbbc8f74
fandi a <fandiiardy@gmail.com>
2024-12-13 19:32:32 +0700

    fixing some errors

--- Backend/src/Controllers/auth.controller.js
+++ Backend/src/Controllers/auth.controller.js
@@ -77,9 +77,9 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = async (req, res) => {
+export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", {maxAge: 0});
+    res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
 
   } catch (error) {
