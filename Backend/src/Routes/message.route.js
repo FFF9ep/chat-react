88b5666681a6dea97e7451194d5098003f148365
fandi a <fandiiardy@gmail.com>
2024-12-15 10:50:17 +0700

    creating send message endpoint

--- Backend/src/Routes/message.route.js
+++ Backend/src/Routes/message.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { protectRoute } from "../Middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar } from "../Controllers/message.controller.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../Controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/user", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
