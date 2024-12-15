3ec44958ce3e469af23d6375afbcd866f04a4f6f
fandi a <fandiiardy@gmail.com>
2024-12-15 15:19:42 +0700

    Updating Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Navbar from './Components/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
 import SignUpPage from './Pages/SignUpPage'
 import LoginPage from './Pages/LoginPage'
@@ -29,11 +29,11 @@ const App = () => {
 
     <Navbar/>
     <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/signup" element={<SignUpPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/" element={ authUser ? <HomePage/> : <Navigate to="/login"/> }/>
+      <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
+      <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
       <Route path="/settings" element={<SettingsPage/>}/>
-      <Route path="/profile" element={<ProfilePage/>}/>
+      <Route path="/profile" element={ authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
     </Routes>
     </div>
   )
